f41f862c68adc9f319f0d064281b99f15dfc6148
swapfM <58875865+swapfM@users.noreply.github.com>
2021-07-08 23:21:52 +0530

    Update Not Adjacent Matrix .cpp

--- codeforces/practise/Not Adjacent Matrix .cpp
+++ codeforces/practise/Not Adjacent Matrix .cpp
@@ -1,1 +1,42 @@
 
+#include<bits/stdc++.h> 
+using namespace std;
+ int main(){
+
+     int t;
+     cin>>t;
+
+     while(t--){
+
+         int n,ans=1,c=1;
+         cin>>n;
+
+         if(n==2){cout<<"-1\n";continue;}
+           int d = (n*n+1)/2;
+
+           for(int i=1;i<=n;i++){
+
+               for(int j=1;j<=n;j++){
+
+                   if(c%2==1){
+                       cout<<ans<<" ";
+                       c++;
+                   }    
+
+                   else{
+                       cout<<ans+d<<" ";
+                       ans++;
+                       c++;
+
+                   }
+                   
+
+
+               }
+               cout<<"\n";
+
+           }
+         }
+
+     
+ }
