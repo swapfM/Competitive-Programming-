6dc87587405999ba7c120a741910a7c447b83a59
swapfM <58875865+swapfM@users.noreply.github.com>
2021-07-08 23:21:19 +0530

    Update Ordinary Numbers .cpp

--- codeforces/practise/Ordinary Numbers .cpp
+++ codeforces/practise/Ordinary Numbers .cpp
@@ -1,1 +1,40 @@
 
+#include<bits/stdc++.h> 
+using namespace std;
+ int main(){
+
+     int t;
+     cin>>t;
+
+     while(t--){
+
+         int n,ans=0;
+         cin>>n;
+            int t = n;
+            int arr[20],c=1;
+         while(1){
+
+             if(n>9){
+             n/=10;
+             ans+=9;
+             c++;
+             }
+             else {
+                 int temp=0;
+                 for(int i=0;i<c;i++){
+                  temp+= pow(10,i);
+                 
+                 }
+
+
+                if(temp*n <= t)
+                    cout<<ans+n<<"\n";
+                 else cout<<ans+n-1<<"\n";
+
+                 break;
+             }
+
+         }
+
+     
+ }}
