5fe509956525a344eb0245da07f6c11d1dd24231
swapfM <58875865+swapfM@users.noreply.github.com>
2021-07-15 23:25:52 +0530

    Update Maximum Cost Deletion.cpp

--- codeforces/practise/Maximum Cost Deletion.cpp
+++ codeforces/practise/Maximum Cost Deletion.cpp
@@ -1,1 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+
+
+
+int main(){
+	
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	
+	int t;
+	cin>>t;
+	
+	while(t--){
+	
+	
+	int n,a,b;
+	cin>>n>>a>>b;
+	
+	string s;
+	cin>>s;
+	
+	
+	
+	int alt = n*(a+b);
+	
+	
+   
+    
+    int t=0;
+    
+    for(int i=1;i<n;i++){
+		
+		if(s[i]!=s[i-1])t++;
+		
+	}
+		
+		t++;
+		
+		int ans = a*n + (t/2 + 1)*b;
+	
+	
+		ans=max(alt,ans);
+		cout<<ans<<"\n";
+	
+
+}}
 
