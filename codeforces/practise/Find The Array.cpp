6488b0222012c737423231f3ee29b09e6e79aea6
swapfM <58875865+swapfM@users.noreply.github.com>
2021-07-15 23:25:04 +0530

    Update Find The Array.cpp

--- codeforces/practise/Find The Array.cpp
+++ codeforces/practise/Find The Array.cpp
@@ -1,1 +1,37 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+
+
+
+int main(){
+	
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	
+	int t;
+	cin>>t;
+	
+	while(t--){
+	
+	
+	int n;
+	cin>>n;
+	
+	if(n==1)cout<<"1\n";
+	else {
+		
+		int t=sqrt(n);
+		if(pow(t,2)==n)cout<<t<<"\n";
+		else cout<<t+1<<"\n";
+		
+		}
+	
+	
+}
+
+}
+
+ 
