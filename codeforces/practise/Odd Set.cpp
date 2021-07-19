97b13af02d4819ae699db4e2c0dcc3116acc9496
swapfM <58875865+swapfM@users.noreply.github.com>
2021-07-19 22:40:42 +0530

    Update Odd Set.cpp

--- codeforces/practise/Odd Set.cpp
+++ codeforces/practise/Odd Set.cpp
@@ -1,1 +1,45 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int mod = 998244353;
+
+
+
+
+void solve(){
+	
+	int n;
+	cin>>n;
+	n*=2;
+	int a=0,b;
+	
+	for(int i=0;i<n;i++){
+		
+		cin>>b;
+		b%2==0 ?a++:a--;
+		
+		}
+		
+		a==0?cout<<"Yes\n":cout<<"No\n";
+	
+	
+	}
+
+
+
+int main(){
+	
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	
+	int t;
+	cin>>t;
+	while(t--){
+		
+		solve();
+		
+		}
+	}
+	
+	
