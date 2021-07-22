db7fc17f0ee62797a306edd2cc08492940f424e7
swapfM <58875865+swapfM@users.noreply.github.com>
2021-07-22 23:25:17 +0530

    Update Digits Sum.cpp

--- codeforces/practise/Digits Sum.cpp
+++ codeforces/practise/Digits Sum.cpp
@@ -1,1 +1,26 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int mod = 998244353;
+
+
+		
+int main()
+{
+	 ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+ int t;
+ cin>>t;
+ while(t--){
+	 
+	 ll n;
+	 cin>>n;
+	 
+	 n++;
+	 n /= 10;
+	 cout<<n<<"\n";
+  
+}
+}
