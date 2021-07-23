30d4a7b3625a69fbcecd9dbcb0f79dab91f02800
swapfM <58875865+swapfM@users.noreply.github.com>
2021-07-23 23:12:07 +0530

    Update Polycarp and Coins.cpp

--- codeforces/practise/Polycarp and Coins.cpp
+++ codeforces/practise/Polycarp and Coins.cpp
@@ -5,36 +5,37 @@ int mod = 998244353;
  
  
  void solve(){
-	string s;
-	cin>>s;
-	
-	int n=s.length();
-	int a[26]={0};
-	
-	for(int i=0; i<n; i++){
 		
-		a[s[i]-97]++;
-		
-		}
-		int r=0,g=0;
-		
-		for(int i=0; i<26; i++){
-			
-			if(a[i]>1){r++;g++;}
-			else if(a[i]==1)
-			{
-				
-				if(r<g)r++;
-				else g++;
-				
-				}
-			
-			}
-			
-			if(r==g)cout<<r<<"\n";
-			else cout<<min(r,g)<<"\n";
+	ll n;
+	cin>>n;
+	
+	ll a,b;
+	
+	a = n/3;
+	b = a;
 	
 	
+	if(n%3 == 1)a++;
+	else if(n%3 ==2)b++;
+	
+	cout<<a<<" "<<b<<"\n";
 	return;
 	
  }
+
+	
+	
+		
+int main()
+{
+	 ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+ int t;
+ cin>>t;
+ while(t--){
+	
+	
+	solve();
+}
+}
