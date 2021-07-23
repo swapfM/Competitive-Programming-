97bd713a410c10259031901b23a879a8b236f0f9
swapfM <58875865+swapfM@users.noreply.github.com>
2021-07-23 23:09:40 +0530

    Update Polycarp and Coins.cpp

--- codeforces/practise/Polycarp and Coins.cpp
+++ codeforces/practise/Polycarp and Coins.cpp
@@ -1,1 +1,42 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int mod = 998244353;
+ 
+ 
+ void solve(){
+		
+	ll n;
+	cin>>n;
+	
+	ll a,b;
+	
+	a = n/3;
+	b = a;
+	
+	
+	if(n%3 == 1)a++;
+	else if(n%3 ==2)b++;
+	
+	cout<<a<<" "<<b<<"\n";
+	return;
+	
+ }
+ 
+	
+	
+		
+int main()
+{
+	 ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+ int t;
+ cin>>t;
+ while(t--){
+	
+	
+	solve();
+}
+}
