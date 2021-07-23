b6ef8fc38cb6fc86a4b1063f984375a078b374ac
swapfM <58875865+swapfM@users.noreply.github.com>
2021-07-23 23:10:43 +0530

    Update Wonderful Coloring - 1.cpp

--- codeforces/practise/Wonderful Coloring - 1.cpp
+++ codeforces/practise/Wonderful Coloring - 1.cpp
@@ -1,1 +1,41 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int mod = 998244353;
+ 
+ 
+ void solve(){
+	string s;
+	cin>>s;
+	
+	int n=s.length();
+	int a[26]={0};
+	
+	for(int i=0; i<n; i++){
+		
+		a[s[i]-97]++;
+		
+		}
+		int r=0,g=0;
+		
+		for(int i=0; i<26; i++){
+			
+			if(a[i]>1){r++;g++;}
+			else if(a[i]==1)
+			{
+				
+				if(r<g)r++;
+				else g++;
+				
+				}
+			
+			}
+			
+			if(r==g)cout<<r<<"\n";
+			else cout<<min(r,g)<<"\n";
+	
+	
+	return;
+	
+ }
