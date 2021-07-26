6075bddd1db9404e6d8c5c142f53151ad9778e3d
swapfM <58875865+swapfM@users.noreply.github.com>
2021-07-26 23:34:18 +0530

    Update Exciting Bets.cpp

--- codeforces/practise/Exciting Bets.cpp
+++ codeforces/practise/Exciting Bets.cpp
@@ -1,1 +1,42 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int mod = 998244353;
+
+
+
+
+void solve(){
+
+		
+		ll a,b;
+		
+		cin>>a>>b;
+		
+		if(a==b)cout<<"0 0\n";
+		else if(a+1==b || b+1==a)cout<<"1 0\n";
+		
+		else {
+			ll d = abs(a-b);
+			
+			ll rr = min((b - (b / d) * d), ((b / d) + (b % d != 0)) * d - b);
+			
+			cout<<d<<" "<<rr<<"\n";
+			
+			
+			
+			
+			
+			}
+		
+					
+					
+					}
+			
+	
+	
+
+
+
+	
