85ae122d5ad62ceca9a6284d58369e04b5039e15
swapfM <58875865+swapfM@users.noreply.github.com>
2021-07-26 23:36:02 +0530

    Update IQ Test.cpp

--- codeforces/practise/IQ Test.cpp
+++ codeforces/practise/IQ Test.cpp
@@ -1,1 +1,62 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int mod = 998244353;
+
+
+
+
+void solve(){
+
+		int n;
+		cin>>n;
+		
+		vector<int>v(n);
+		for(int i=0; i<n; i++)cin>>v[i];
+		int e=0,o=0;
+		
+		
+		for(int i=0; i<3; i++){
+			
+			if(v[i]%2==0)e++;
+			else o++;
+			}
+			if(e>o){
+			
+			for(int i=0; i<n;i++){
+				
+				if(v[i]%2==1){cout<<i+1<<"\n";return;}
+				
+				}}
+				
+else {
+					
+					
+			for(int i=0; i<n;i++){
+				
+				if(v[i]%2==0){cout<<i+1<<"\n";return;}
+				
+				}}
+					
+					
+					}
+			
+	
+	
+
+
+
+int main(){
+	
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	
+	
+		
+		solve();
+		
+		
+	}
+	
+	
