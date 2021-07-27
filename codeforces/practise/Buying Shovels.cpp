e4107b7b4efdd33c5d7e436818852b496cf584c2
swapfM <58875865+swapfM@users.noreply.github.com>
2021-07-27 23:45:24 +0530

    Update Buying Shovels.cpp

--- codeforces/practise/Buying Shovels.cpp
+++ codeforces/practise/Buying Shovels.cpp
@@ -1,12 +1,52 @@
 
-#include<bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-int mod = 998244353;
  
- 
+ void solve(){
+	
+	ll n,k;
+	
+	cin>>n>>k;
+	if(n<=k){cout<<"1\n";return;}
+	
+	int ans = n;
+	ll t = sqrt(n);
+	
+	for(int i=2; i<=k&&i<=t; i++){
+		
+		
+		if(n%i==0){
+			int t;
+			int x = n/i;
+			if(x > k)t = x;
+			else t = min(x,i);
+			
+			ans = min(t,ans);
+			}
+		
+		
+		}
+		cout<<ans<<"\n";
+		
+	return;
+	
+ }
 
+	
+	
+		
+int main()
+{
+	 ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
  
+ int t;
+ cin>>t;
+ while(t--){
+	
+	
+	solve();
+}
+}
+
 	
 	
 		
