81b14d8d0d0ca38fbe532fd4b62400be8ec68aa4
swapfM <58875865+swapfM@users.noreply.github.com>
2021-07-27 23:45:09 +0530

    Update Buying Shovels.cpp

--- codeforces/practise/Buying Shovels.cpp
+++ codeforces/practise/Buying Shovels.cpp
@@ -5,36 +5,8 @@ typedef long long ll;
 int mod = 998244353;
  
  
- void solve(){
-	
-	ll n,k;
-	
-	cin>>n>>k;
-	if(n<=k){cout<<"1\n";return;}
-	
-	int ans = n;
-	ll t = sqrt(n);
-	
-	for(int i=2; i<=k&&i<=t; i++){
-		
-		
-		if(n%i==0){
-			int t;
-			int x = n/i;
-			if(x > k)t = x;
-			else t = min(x,i);
-			
-			ans = min(t,ans);
-			}
-		
-		
-		}
-		cout<<ans<<"\n";
-		
-	return;
-	
- }
 
+ 
 	
 	
 		
