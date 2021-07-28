b739c38daf8e939eaa87946a9f7352cdbdc0d98c
swapfM <58875865+swapfM@users.noreply.github.com>
2021-07-28 23:21:58 +0530

    Update Chef In Infinite Plane.cpp

--- codechef/Chef In Infinite Plane.cpp
+++ codechef/Chef In Infinite Plane.cpp
@@ -1,1 +1,48 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int mod = 998244353;
+ 
+ 
+ void solve(){
+	
+	int n;
+	cin>>n;
+	
+	vector<int>v(n);
+	ll ans = 0,sel=0;
+	unordered_map<int,int>mp;
+	
+	for(int i=0; i<n; i++){
+		
+		cin>>v[i];
+		mp[v[i]]++;
+		
+		}
+		
+		sort(v.begin(), v.end());
+		
+		for(int i=0; i<n; i++){
+			
+			
+			if(mp[v[i]] >= (v[i]-1)){
+				
+				ans = ans+ v[i]-1;
+				
+				}
+				else ans += mp[v[i]];
+				i = i+mp[v[i]]-1;
+			
+			
+			}
+			cout<<ans<<"\n";
+		
+		
+	return;
+	
+ }
+
+	
+	
+	
