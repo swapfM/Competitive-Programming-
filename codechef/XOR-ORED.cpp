fbcd3ab13c7a611d7daa77d021dc4e29083e6868
swapfM <58875865+swapfM@users.noreply.github.com>
2021-07-28 23:21:27 +0530

    Update XOR-ORED.cpp

--- codechef/XOR-ORED.cpp
+++ codechef/XOR-ORED.cpp
@@ -1,1 +1,52 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int mod = 998244353;
+ 
+ 
+ void solve(){
+	
+	int n;
+	cin>>n;
+	
+	vector<int>v(n);
+	ll ans = 0,sel=0;
+	unordered_map<int,int>mp;
+	
+	for(int i=0; i<n; i++){
+		
+		cin>>v[i];
+		mp[v[i]]++;
+		
+		}
+		
+		int rep = 0;
+		
+		for(int i=0; i<n; i++){
+			
+			if(mp[v[i]] > rep){
+				
+				sel = v[i];
+				rep = mp[v[i]];
+				}
+			
+			}
+			
+			for(int i=0; i<n; i++){
+				
+				
+				ll xr = v[i] ^ sel;
+				ans |= xr;
+				
+				
+				}
+				
+				cout<<sel<<" "<<ans<<"\n";
+		
+	return;
+	
+ }
+
+	
+	
