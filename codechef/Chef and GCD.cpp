61afbf95bacdba77209d993a5e566c110c611a0d
swapfM <58875865+swapfM@users.noreply.github.com>
2021-07-28 23:21:01 +0530

    Update Chef and GCD.cpp

--- codechef/Chef and GCD.cpp
+++ codechef/Chef and GCD.cpp
@@ -1,1 +1,54 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int mod = 998244353;
+ 
+ 
+ void solve(){
+	
+	ll x,y;
+	cin>>x>>y;
+	
+	int res = 0;
+	if(x==1){
+		
+		res++;
+		x++;
+		
+		}
+		if(y==1){
+			
+			
+			res++;
+			y++;
+			
+			
+			}
+			
+			if(__gcd(x,y) > 1){
+				
+				cout<<res<<endl;
+				
+				}
+				else {
+					
+					if(x%2 == 1 && y%2 == 1){
+						
+						if(__gcd(x+1,y) >1)cout<<res+1<<endl;
+						else if(__gcd(x,y+1)>1)cout<<res+1<<endl;
+						else cout<<res+2<<endl;
+						
+						
+						}
+						else cout<<res+1<<endl;
+					
+					
+					}
+		
+	return;
+	
+ }
+
+	
+	
