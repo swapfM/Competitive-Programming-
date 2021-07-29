a0496bdbce95d22daf6b497da8e94a73693e8000
swapfM <58875865+swapfM@users.noreply.github.com>
2021-07-29 23:18:18 +0530

    Update Cherry.cpp

--- codeforces/practise/Cherry.cpp
+++ codeforces/practise/Cherry.cpp
@@ -1,1 +1,47 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int mod = 998244353;
+ 
+ 
+ void solve(){
+	
+	int n;
+	cin>>n;
+	
+	vector<ll>v(n);
+	ll ans = 0,t=0;
+	
+	for(int i=0; i<n; i++)cin>>v[i];
+	
+	for(int i=1; i<n; i++){
+		
+		t = v[i] * v[i-1];
+		
+		ans = max(t,ans);
+		
+		
+		}
+		cout<<ans<<"\n";
+		
+	return;
+	
+ }
+
+	
+	
+		
+int main()
+{
+	 ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+ int t;
+ cin>>t;
+ while(t--){
+	
+	
+	solve();
+}
+}
