d42de896bc2ded79094f60b0acf279a25a0414f8
swapfM <58875865+swapfM@users.noreply.github.com>
2021-07-30 23:15:35 +0530

    Update Cobb.cpp

--- codeforces/practise/Cobb.cpp
+++ codeforces/practise/Cobb.cpp
@@ -1,1 +1,48 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int mod = 998244353;
+ 
+ 
+ void solve(){
+	
+	int n,k;
+	cin>>n>>k;
+	
+	vector<int>v(n+1);
+	
+	for(int i=1; i<=n; i++)cin>>v[i];
+	
+	ll ans = -99999999;
+	
+	for(int i=n; i>0; i--){
+		
+		
+		for(int j = i-1; j>0; j--){
+			
+			
+			ll t = i;
+			t *= j;
+			t -= k*(v[i] | v[j]);
+			
+			ans = max(ans,t);
+			
+			ll x = i;
+			x*= (j-1);
+			
+			if(ans > x){break;}
+			
+			
+			}
+		
+		
+		
+		}
+		cout<<ans<<"\n";
+ }
+
+	
+	
+		
+
