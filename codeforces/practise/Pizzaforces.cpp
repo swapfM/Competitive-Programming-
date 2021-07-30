34a9f4bdc68a950925f538fa618fe41700c4a459
swapfM <58875865+swapfM@users.noreply.github.com>
2021-07-30 23:12:01 +0530

    Update Pizzaforces.cpp

--- codeforces/practise/Pizzaforces.cpp
+++ codeforces/practise/Pizzaforces.cpp
@@ -1,1 +1,92 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int mod = 1e9 + 7;
+
+
+ll func1(ll n) {
+
+	ll tt = n / 6;
+	tt *=  15;
+
+	if (n % 6 != 0)tt += 15;
+
+	return tt;
+}
+
+
+ll func2(ll n) {
+
+	ll tt = n / 8;
+	tt *=  20;
+
+	if (n % 8 != 0) {
+
+		if ((n % 8) < 7)tt += 15;
+		else tt += 20;
+	}
+
+	return tt;
+
+}
+
+ll func3(ll n) {
+
+	ll tt = n / 10;
+	tt *=  25;
+
+	if (n % 10 != 0) {
+
+		ll rem = n % 10;
+
+		if (rem <= 6)tt += 15;
+
+		else if (rem > 7 && rem <= 8)tt += 20;
+		else if (rem == 9)tt += 25;
+	}
+
+	return tt;
+
+}
+
+
+void solve() {
+
+	ll n;
+	cin >> n;
+
+	// ll ans = 0;
+
+	// ll t6 = func1(n);
+
+	// ll t8 = func2(n);
+
+	// ll t10 = func3(n);
+
+	// ans = min(t6, t8);
+
+	// ans = min(ans, t10);
+
+	// cout << ans << "\n";
+	ll alt = 0;
+
+	if (n <= 6) {
+		alt = 15;
+	}
+	else {
+		if (n % 2 != 0)n++;
+		alt = (n * 5) / 2;
+	}
+
+	// ans = min(alt, ans);
+	cout << alt << "\n";
+
+
+
+}
+
+
+
+
+
