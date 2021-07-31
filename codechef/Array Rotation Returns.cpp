7804094887a5b8cd3a0ae03b150f85c0b11d73bc
swapfM <58875865+swapfM@users.noreply.github.com>
2021-07-31 23:11:49 +0530

    Update Array Rotation Returns.cpp

--- codechef/Array Rotation Returns.cpp
+++ codechef/Array Rotation Returns.cpp
@@ -114,10 +114,24 @@ void solve() {
 	cout << "\n";
 
 
+}
 
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	int t;
+	cin >> t;
+	while (t--) {
 
+		solve();
 
+	}
 }
 
 
