15f37ba99cd54cb05ae7a12531f73be2f3c0e6a0
swapfM <58875865+swapfM@users.noreply.github.com>
2021-07-31 23:09:36 +0530

    Update Beautiful Pairs.cpp

--- codechef/Beautiful Pairs.cpp
+++ codechef/Beautiful Pairs.cpp
@@ -1,1 +1,42 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve() {
+
+	int n;
+	cin >> n;
+	ll res = 0;
+	ll tt = 0;
+
+
+	unordered_map<int, int>mp;
+
+	vector<int>v(n);
+
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+		mp[v[i]]++;
+	}
+
+
+
+	for (int i = 0; i < n - 1; i++) {
+
+		tt = n - i - mp[v[i]] ;
+
+		res += tt;
+		mp[v[i]]--;
+	}
+
+	res *= 2;
+
+	cout << res << "\n";
+
+
+
+}
+
+
+
