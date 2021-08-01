644af84ebe118c6e85f2c9962f115712a635059d
swapfM <58875865+swapfM@users.noreply.github.com>
2021-08-01 23:28:39 +0530

    Update Gregor and Cryptography.cpp

--- codeforces/practise/Gregor and Cryptography.cpp
+++ codeforces/practise/Gregor and Cryptography.cpp
@@ -1,1 +1,22 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i,n) for (int i = 0; i < n; ++i)
+#define REP(i,k,n) for (int i = k; i <= n; ++i)
+#define REPR(i,k,n) for (int i = k; i >= n; --i)
+int mod = 1e7 + 9;
+
+
+
+void solve() {
+
+	ll n;
+	cin >> n;
+
+	cout << 2 << " " << n - 1 << "\n";
+
+
+
+}
+
