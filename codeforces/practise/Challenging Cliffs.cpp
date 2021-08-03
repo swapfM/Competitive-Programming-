100226c55af692a4ed630bc66cee616efe762600
swapfM <58875865+swapfM@users.noreply.github.com>
2021-08-03 22:56:28 +0530

    Update Challenging Cliffs.cpp

--- codeforces/practise/Challenging Cliffs.cpp
+++ codeforces/practise/Challenging Cliffs.cpp
@@ -1,36 +1,6 @@
 
 
-void solve(){
-
-	int n;
-	cin>>n;
-	vector<int>v(n);
-	
-	for(int i=0; i<n; i++)cin>>v[i];
-	
-	sort(v.begin(), v.end());
-	
-	int a=0,b=0,f=999999999;
-	
-	for(int i=1;i<n;i++){
-		
-		
-		if(f>v[i]-v[i-1])a=i-1,b=i,f=v[i]-v[i-1];
-		
-		
-		}
-		
-		cout<<v[a]<<" ";
-	
-	
-	for(int i=0; i<n; i++){
-		
-		if(i==a || i==b)continue;
-		
-		n%2==0? cout<<v[i] : cout<<v[n-1-i];
-		cout<<" ";
-		
-		}
+}
 		cout<<v[b]<<"\n";
 	
 		return;
