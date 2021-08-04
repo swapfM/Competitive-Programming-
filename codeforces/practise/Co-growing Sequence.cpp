50be1a146fdbeaf84e06c6aa5d4f041378c9a000
swapfM <58875865+swapfM@users.noreply.github.com>
2021-08-04 22:47:15 +0530

    Update Co-growing Sequence.cpp

--- codeforces/practise/Co-growing Sequence.cpp
+++ codeforces/practise/Co-growing Sequence.cpp
@@ -1,13 +1,4 @@
 
-#include<bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-#define rep(i,n) for (int i = 0; i < n; ++i)
-#define REP(i,k,n) for (int i = k; i <= n; ++i)
-#define REPR(i,k,n) for (int i = k; i >= n; --i)
-ll mod = 1e10 + 9;
-
-
 
 void solve()
 {
@@ -48,4 +39,21 @@ void solve()
 
 
 
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	int t;
+	cin >> t;
+	while (t--) {
+
+		solve();
+
+	}
+}
 
