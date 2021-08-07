c2ba9aa17bcb82e509931541f88e48f20a9e56da
swapfM <58875865+swapfM@users.noreply.github.com>
2021-08-07 22:44:39 +0530

    Update Problem Difficulties.cpp

--- codechef/Problem Difficulties.cpp
+++ codechef/Problem Difficulties.cpp
@@ -1,4 +1,11 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i,n) for (int i = 0; i < n; ++i)
+#define REP(i,k,n) for (int i = k; i <= n; ++i)
+#define REPR(i,k,n) for (int i = k; i >= n; --i)
+
 
 
 void solve() {
@@ -17,27 +24,6 @@ void solve() {
 	else if (a == 3)cout << "1\n";
 	else cout << "0\n";
 	return;
-
-}
-
-
-
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-
-#ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-#endif
-
-	int t;
-	cin >> t;
-	while (t--) {
-		solve();
-	}
-
 
 }
 
