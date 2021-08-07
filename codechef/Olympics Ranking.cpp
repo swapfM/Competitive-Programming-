3ab511503028089c6dadcd2e43abe3f60eb01f70
swapfM <58875865+swapfM@users.noreply.github.com>
2021-08-07 22:45:04 +0530

    Update Olympics Ranking.cpp

--- codechef/Olympics Ranking.cpp
+++ codechef/Olympics Ranking.cpp
@@ -1,1 +1,36 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i,n) for (int i = 0; i < n; ++i)
+#define REP(i,k,n) for (int i = k; i <= n; ++i)
+#define REPR(i,k,n) for (int i = k; i >= n; --i)
+
+
+
+void solve() {
+
+	vector<int>v(3);
+
+	int s1 = 0, s2 = 0;
+
+	rep(i, 3) {
+		cin >> v[i];
+		s1 += v[i];
+	}
+
+	rep(i, 3) {
+		cin >> v[i];
+		s2 += v[i];
+	}
+
+	s1 > s2 ? cout << "1\n" : cout << "2\n";
+
+	return;
+
+}
+
+
+
+
+
