5ac014595805685a36bd7f5831b050b2b66ab251
swapfM <58875865+swapfM@users.noreply.github.com>
2021-08-07 22:45:13 +0530

    Update Olympics Ranking.cpp

--- codechef/Olympics Ranking.cpp
+++ codechef/Olympics Ranking.cpp
@@ -29,6 +29,24 @@ void solve() {
 	return;
 
 }
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int t;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+
+
+}
 
 
 
