a6a3f68cdf754173c6f929d0cba5f39034ef49a7
swapfM <58875865+swapfM@users.noreply.github.com>
2021-08-08 23:10:26 +0530

    Update Chef and Invention.cpp

--- codechef/Chef and Invention.cpp
+++ codechef/Chef and Invention.cpp
@@ -9,6 +9,33 @@ typedef long long ll;
 
 
 
+void solve() {
+
+	vector<ll>v(3);
+
+	rep(i, 3)cin >> v[i];
+
+	ll ans = 0, tt = 0;
+
+	ans = v[1] % v[2];
+
+	tt = (v[0] - 1) / v[2];
+
+	tt++;
+
+	ans *= tt;
+
+	tt = v[1] / v[2];
+
+	ans += 1 + tt;
+	cout << ans << "\n";
+
+	return;
+
+
+
+
+}
 
 
 int main()
