0a89de719229a0e8d672afaec00e3b34a6cf04c3
swapfM <58875865+swapfM@users.noreply.github.com>
2021-08-08 23:10:12 +0530

    Update Chef and Invention.cpp

--- codechef/Chef and Invention.cpp
+++ codechef/Chef and Invention.cpp
@@ -8,30 +8,24 @@ typedef long long ll;
 
 
 
-void solve() {
-
-	vector<ll>v(3);
-
-	rep(i, 3)cin >> v[i];
-
-	ll ans = 0, tt = 0;
-
-	ans = v[1] % v[2];
-
-	tt = (v[0] - 1) / v[2];
-
-	tt++;
-
-	ans *= tt;
-
-	tt = v[1] / v[2];
-
-	ans += 1 + tt;
-	cout << ans << "\n";
-
-	return;
 
 
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int t;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
 
 
 }
