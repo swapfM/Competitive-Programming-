369a99020c4dfb578d1157b1d10d64a2e437323d
swapfM <58875865+swapfM@users.noreply.github.com>
2021-08-08 23:09:53 +0530

    Update Chef and Invention.cpp

--- codechef/Chef and Invention.cpp
+++ codechef/Chef and Invention.cpp
@@ -1,4 +1,12 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i,n) for (int i = 0; i < n; ++i)
+#define REP(i,k,n) for (int i = k; i <= n; ++i)
+#define REPR(i,k,n) for (int i = k; i >= n; --i)
+
+
 
 void solve() {
 
@@ -28,25 +36,5 @@ void solve() {
 
 }
 
-
-
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-
-#ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-#endif
-
-	int t;
-	cin >> t;
-	while (t--) {
-		solve();
-	}
-
-
-}
 
 
