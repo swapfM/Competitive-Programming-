56a081250bd0a0da7a7108cd61891df7935a4b99
swapfM <58875865+swapfM@users.noreply.github.com>
2021-08-10 23:49:50 +0530

    Update Ezzat and Two Subsequences.cpp

--- codeforces/practise/Ezzat and Two Subsequences.cpp
+++ codeforces/practise/Ezzat and Two Subsequences.cpp
@@ -1,1 +1,17 @@
 
+int main()
+{
+	ll t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int ar[n];
+		ll sum=0;
+		for(int i=0;i<n;i++)
+		cin>>ar[i],sum+=ar[i];
+		sort(ar,ar+n);
+		cout<<fixed<<setprecision(10)<<(sum-ar[n-1])/(n-1)+ar[n-1]<<"\n";
+	}
+}
