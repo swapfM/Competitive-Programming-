2557980afdc32dabd1a042631531e774802c17c5
swapfM <58875865+swapfM@users.noreply.github.com>
2021-08-13 23:21:38 +0530

    Update Ezzat and Two Subsequences.cpp

--- codeforces/practise/Ezzat and Two Subsequences.cpp
+++ codeforces/practise/Ezzat and Two Subsequences.cpp
@@ -31,3 +31,23 @@ void solve() {
 
 
 
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int t;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+
+
+}
+
+
