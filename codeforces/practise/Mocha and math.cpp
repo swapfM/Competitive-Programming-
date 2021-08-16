5ebb6749869d615d5c61ed83f6c1d725d88688b5
swapfM <58875865+swapfM@users.noreply.github.com>
2021-08-16 23:19:55 +0530

    Update Mocha and math.cpp

--- codeforces/practise/Mocha and math.cpp
+++ codeforces/practise/Mocha and math.cpp
@@ -1,4 +1,9 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i,n) for (int i = 0; i < n; ++i)
+#define REP(i,k,n) for (int i = k; i <= n; ++i)
+#define REPR(i,k,n) for (int i = k; i >= n; --i)
 
 
 
@@ -21,6 +26,27 @@ void solve() {
 
 	cout << ans << "\n";
 	return;
+
+
+}
+
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int t;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
 
 
 }
