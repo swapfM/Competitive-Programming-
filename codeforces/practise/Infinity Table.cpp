27070ddf91de02ade3bdb4709e75d3f0ec885bc8
swapfM <58875865+swapfM@users.noreply.github.com>
2021-08-18 23:40:12 +0530

    Update Infinity Table.cpp

--- codeforces/practise/Infinity Table.cpp
+++ codeforces/practise/Infinity Table.cpp
@@ -1,3 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i,n) for (int i = 0; i < n; ++i)
+#define REP(i,k,n) for (int i = k; i <= n; ++i)
+#define REPR(i,k,n) for (int i = k; i >= n; --i)
+
 
 
 void solve() {
@@ -17,11 +24,32 @@ void solve() {
 	ll rem = n - s;
 
 
-	
+	if (rem <= r)cout << rem << " " << r << "\n";
+	else cout << r << " " << 2 * r - rem << "\n";
 	return;
 
 
 }
 
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int t;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+
+
+}
 
 
