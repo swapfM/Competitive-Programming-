cc80d27976ec50ff751c5cafcca42180bce4df8a
swapfM <58875865+swapfM@users.noreply.github.com>
2021-08-21 23:39:54 +0530

    Update Pleasant Pairs.cpp

--- codeforces/practise/Pleasant Pairs.cpp
+++ codeforces/practise/Pleasant Pairs.cpp
@@ -1,3 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i,n) for (int i = 0; i < n; ++i)
+#define REP(i,k,n) for (int i = k; i <= n; ++i)
+#define REPR(i,k,n) for (int i = k; i >= n; --i)
+
 
 
 void solve() {
@@ -31,4 +38,25 @@ void solve() {
 
 
 }
+
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int t;
+	cin >> t;
+	while (t--)
+		solve();
+
+
+}
+
 
