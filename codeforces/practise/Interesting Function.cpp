9d021b7885edf4bf45e8c70c8c205cb803e37ed7
swapfM <58875865+swapfM@users.noreply.github.com>
2021-08-23 23:29:37 +0530

    Update Interesting Function.cpp

--- codeforces/practise/Interesting Function.cpp
+++ codeforces/practise/Interesting Function.cpp
@@ -1,1 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i,n) for (int i = 0; i < n; ++i)
+#define REP(i,k,n) for (int i = k; i <= n; ++i)
+#define REPR(i,k,n) for (int i = k; i >= n; --i)
 
+
+
+
+void solve() {
+
+	ll a, b;
+	cin >> a >> b;
+
+	ll d = b - a;
+
+	ll ans = d;
+
+	string x = to_string(a);
+	string y = to_string(b);
+	int pre = ans;
+	int sd = y.size() - x.size();
+
+	rep(i, sd)x = "0" + x;
+
+	REPR(i, y.size() - 2, 0) {
+
+		ll tt = int(x[i + 1] - 48);
+		tt += pre;
+		tt /= 10;
+		ans += tt;
+		pre = tt;
+	}
+
+	cout << ans << "\n";
+
+	return;
+
+}
+
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int t;
+	cin >> t;
+	while (t--)
+		solve();
+
+
+}
