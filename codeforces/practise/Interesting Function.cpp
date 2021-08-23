67810381b16fbce54041069643833b6f9a27105f
swapfM <58875865+swapfM@users.noreply.github.com>
2021-08-23 23:30:00 +0530

    Update Interesting Function.cpp

--- codeforces/practise/Interesting Function.cpp
+++ codeforces/practise/Interesting Function.cpp
@@ -1,3 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i,n) for (int i = 0; i < n; ++i)
+#define REP(i,k,n) for (int i = k; i <= n; ++i)
+#define REPR(i,k,n) for (int i = k; i >= n; --i)
+
 
 
 
@@ -32,3 +39,22 @@ void solve() {
 
 }
 
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int t;
+	cin >> t;
+	while (t--)
+		solve();
+
+
+}
