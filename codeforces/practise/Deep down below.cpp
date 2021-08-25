f263b5ba3fa7f4fc221459dd6d61ac41c765ae17
swapfM <58875865+swapfM@users.noreply.github.com>
2021-08-25 23:57:14 +0530

    Update Deep down below.cpp

--- codeforces/practise/Deep down below.cpp
+++ codeforces/practise/Deep down below.cpp
@@ -1,3 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i,n) for (int i = 0; i < n; ++i)
+#define REP(i,k,n) for (int i = k; i <= n; ++i)
+#define REPR(i,k,n) for (int i = k; i >= n; --i)
 
 
 
@@ -34,10 +40,32 @@ void solve() {
 		sort(m.begin(),m.end());
 
 		int ans = 0;
+
+		
+
+
+
+
 	
 
 }
 
 
 
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int t;
+	cin >> t;
+	while (t--)
+		solve();
+
+
+}
