9f54db0accbb6ec81f27977697419483f847ce29
swapfM <58875865+swapfM@users.noreply.github.com>
2021-08-29 23:25:45 +0530

    Update Just a Graph .cpp

--- codechef/Just a Graph .cpp
+++ codechef/Just a Graph .cpp
@@ -1,1 +1,46 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i,n) for (int i = 0; i < n; ++i)
+#define REP(i,k,n) for (int i = k; i <= n; ++i)
+#define REPR(i,k,n) for (int i = k; i >= n; --i)
+
+
+
+
+
+void solve() {
+
+	int n;
+	cin >> n;
+
+	vector<ll>v(n);
+	unordered_map<ll, ll>mp;
+
+	rep(i, n) {
+		cin >> v[i];
+		v[i] -= (i + 1);
+		mp[v[i]]++;
+	}
+
+	ll ans = 1;
+
+	rep(i, n - 1) {
+
+		if (mp[v[i]] == n - i )ans++;
+
+		mp[v[i]]--;
+
+	}
+
+	cout << ans << "\n";
+
+
+
+
+}
+
+
+
+
