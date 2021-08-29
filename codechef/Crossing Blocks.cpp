1d1df69871ce360ecd263b8d4b64286aade374b2
swapfM <58875865+swapfM@users.noreply.github.com>
2021-08-29 23:27:18 +0530

    Update Crossing Blocks.cpp

--- codechef/Crossing Blocks.cpp
+++ codechef/Crossing Blocks.cpp
@@ -1,3 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i,n) for (int i = 0; i < n; ++i)
+#define REP(i,k,n) for (int i = k; i <= n; ++i)
+#define REPR(i,k,n) for (int i = k; i >= n; --i)
 
 
 
@@ -36,6 +42,29 @@ void solve() {
 
 	cout << ans << "\n";
 
+
+
+
+}
+
+
+
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int t;
+	cin >> t;
+	while (t--)
+		solve();
 
 
 }
