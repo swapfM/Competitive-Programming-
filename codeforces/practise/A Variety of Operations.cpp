f2c27ccc1db9a4a5113290d9f3fd52b24656aaba
swapfM <58875865+swapfM@users.noreply.github.com>
2021-08-30 23:20:28 +0530

    Update A Variety of Operations.cpp

--- codeforces/practise/A Variety of Operations.cpp
+++ codeforces/practise/A Variety of Operations.cpp
@@ -1,1 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i,n) for (int i = 0; i < n; ++i)
+#define REP(i,k,n) for (int i = k; i <= n; ++i)
+#define REPR(i,k,n) for (int i = k; i >= n; --i)
 
+
+
+void solve() {
+
+	ll a, b;
+	cin >> a >> b;
+
+
+	ll d = b - a;
+
+	if (d % 2 != 0)cout << "-1\n";
+	else if (a + b == 0)cout << "0\n";
+	else if (d == 0)cout << "1\n";
+	else cout << "2\n";
+
+
+
+}
