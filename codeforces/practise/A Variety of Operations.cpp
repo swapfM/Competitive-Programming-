ab1adc8583b1de9250688bed7ac557bfb47e6861
swapfM <58875865+swapfM@users.noreply.github.com>
2021-08-30 23:20:37 +0530

    Update A Variety of Operations.cpp

--- codeforces/practise/A Variety of Operations.cpp
+++ codeforces/practise/A Variety of Operations.cpp
@@ -1,9 +1,3 @@
-#include<bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-#define rep(i,n) for (int i = 0; i < n; ++i)
-#define REP(i,k,n) for (int i = k; i <= n; ++i)
-#define REPR(i,k,n) for (int i = k; i >= n; --i)
 
 
 
@@ -23,3 +17,20 @@ void solve() {
 
 
 }
+
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+
+
+}
+
+
